f2824983004b38d4e55719840681ed80b787147a
Will <doggy.huang@gmail.com>
2018-08-05 05:51:01 +0800

    修正啟動時的一些小錯誤 & 更新安裝檔 (Install.bat)

--- Install.bat
+++ Install.bat
@@ -2,8 +2,15 @@
 
 REM choco install clink -y
 
-SET TargetFolder="c:\Program Files (x86)\clink\0.4.9\modules\"
+SET TargetFolder="c:\Program Files (x86)\clink\0.4.9"
 
-XCOPY /E modules %TargetFolder %
-XCOPY /E spec %TargetFolder%
-XCOPY /E dotnetcli21.lua %TargetFolder%
+XCOPY /E /Y modules           %TargetFolder%\modules\
+XCOPY /E /Y spec              %TargetFolder%\spec\
+XCOPY /E /Y dotnetcli21.lua   %TargetFolder%\
+XCOPY /E /Y npm.lua           %TargetFolder%\
+XCOPY /E /Y angular-cli-6.lua %TargetFolder%\
+
+XCOPY /E /Y .busted     %TargetFolder%\
+XCOPY /E /Y .init.lua   %TargetFolder%\
+XCOPY /E /Y .luacheckrc %TargetFolder%\
+XCOPY /E /Y .luacov     %TargetFolder%\
