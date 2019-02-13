376615f85908fddca791eb957ce0cde27fb6ea3e
Will <doggy.huang@gmail.com>
2019-02-14 01:21:47 +0800

    將幾個常用的 *.lua 加入安裝批次檔中
    chocolatey.lua
    git.lua
    git_prompt.lua
    net.lua
    ssh.lua
    yarn.lua

--- Install.bat
+++ Install.bat
@@ -2,15 +2,21 @@
 
 REM choco install clink -y
 
-SET TargetFolder="c:\Program Files (x86)\clink\0.4.9"
+SET TargetFolder="%ProgramFiles(x86)%\clink\0.4.9"
 
 XCOPY /E /Y modules           %TargetFolder%\modules\
 XCOPY /E /Y spec              %TargetFolder%\spec\
 XCOPY /E /Y dotnetcli21.lua   %TargetFolder%\
 XCOPY /E /Y npm.lua           %TargetFolder%\
 XCOPY /E /Y angular-cli-6.lua %TargetFolder%\
+XCOPY /E /Y chocolatey.lua    %TargetFolder%\
+XCOPY /E /Y git.lua           %TargetFolder%\
+XCOPY /E /Y git_prompt.lua    %TargetFolder%\
+XCOPY /E /Y net.lua           %TargetFolder%\
+XCOPY /E /Y ssh.lua           %TargetFolder%\
+XCOPY /E /Y yarn.lua          %TargetFolder%\
 
-XCOPY /E /Y .busted     %TargetFolder%\
-XCOPY /E /Y .init.lua   %TargetFolder%\
-XCOPY /E /Y .luacheckrc %TargetFolder%\
-XCOPY /E /Y .luacov     %TargetFolder%\
+XCOPY /E /Y .busted           %TargetFolder%\
+XCOPY /E /Y .init.lua         %TargetFolder%\
+XCOPY /E /Y .luacheckrc       %TargetFolder%\
+XCOPY /E /Y .luacov           %TargetFolder%\
